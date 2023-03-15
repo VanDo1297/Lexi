7ab7ecbe88d0139adf6c0c3c6e8f115d936cbee9
dongo <ngovando261297@gmail.com>
2023-03-15 17:37:31 +0700

    test

--- src/components/seo/index.tsx
+++ src/components/seo/index.tsx
@@ -21,6 +21,14 @@ const SEOComponent = ({title, description, shareImage} :{title?:string,descripti
                 <meta name="twitter:image"  content={"https:"+ shareImage} />
                 <meta property="og:url"  content={"https:"+ shareImage} ></meta>
             </>}
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://metatags.io/"/>
+        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"></meta>
         </Head>
     )
 }
