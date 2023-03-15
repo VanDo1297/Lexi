6ae2cf5648039151b4ac58f2e55496f759e89ed3
dongo <ngovando261297@gmail.com>
2023-03-15 17:11:17 +0700

    update SEO

--- src/components/seo/index.tsx
+++ src/components/seo/index.tsx
@@ -14,11 +14,12 @@ const SEOComponent = ({title, description, shareImage} :{title?:string,descripti
             <meta property="og:description" content={description || "Lexi app description"} />
             
             {shareImage && <>
-                <meta property="og:image" content={"https"+ shareImage} />
-                <meta property="twitter:image" content={"https"+ shareImage}  />
-                <meta property="image"  content={"https"+ shareImage} />
-                <meta name="og:image"  content={"https"+ shareImage}  />
-                <meta name="twitter:image"  content={"https"+ shareImage} />
+                <meta property="og:image" content={"https:"+ shareImage} />
+                <meta property="twitter:image" content={"https:"+ shareImage}  />
+                <meta property="image"  content={"https:"+ shareImage} />
+                <meta name="og:image"  content={"https:"+ shareImage}  />
+                <meta name="twitter:image"  content={"https:"+ shareImage} />
+                <meta property="og:url"  content={"https:"+ shareImage} ></meta>
             </>}
         </Head>
     )
