97884e8287f9a4075526a1adc9dedb8dd8bae638
do-ift <61582891+do-ift@users.noreply.github.com>
2023-03-16 18:38:25 +0700

    update

--- src/components/seo/index.tsx
+++ src/components/seo/index.tsx
@@ -37,3 +37,4 @@ const SEOComponent = ({title, description, shareImage} :{title?:string,descripti
 }
 
 export default SEOComponent;
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,8 +34,23 @@ export default function Home(props:IProps) {
                 <div className="sm:block flex mt-auto sm:mb-[40px]">
                     <div className="flex w-full xl:flex-row flex-col justify-between sm:mt-[40px] mt-[100px] self-end">
                         <div className="flex flex-col sm:text-start text-center xl:pl-[48px] xl:pb-[0] pb-[32px] sm:pl-[16px]">
-                            <p className='sm:text-[56px] text-[32px] font-[300] leading-[1em]'>A <span className='italic'>personalized</span>  news feed </p>
-                            <p className="sm:text-[56px] text-[32px] font-[300] leading-[1em]">powered by artificial intelligence</p>
+                            <p className='sm:text-[56px] text-[32px] font-[300] leading-[1em]'>A <span className='italic'>personalized</span> language learning experience powered by artificial intelligence.  </p>
+                            <p className="sm:text-[56px] text-[32px] font-[300] leading-[1em]">Revolutionizing Language Learning for a Connected World </p>
+
+                            <p className="sm:text-[20px] text-[15px] sm:mt-[32px] mt-[16px] sm:mb-[48px] mb-[32px] font-[400] whitespace-pre-line leading-[24px]">{`
+                                 I. Introduction
+
+                                 Lexi is a dynamic, adaptive language learning product that empowers users to learn new languages seamlessly and effectively. Our cutting-edge technology makes language learning an enjoyable and rewarding experience. Our brand personality and mascot embody the spirit of curiosity, adaptability, and constant growth.
+                                 
+                                 II. Brand Personality
+                                 
+                                 Adaptive: Lexi is constantly learning and evolving to meet the unique needs of each user, ensuring a personalized and effective learning experience.
+                                 Empowering: Lexi instills confidence in users by providing the necessary tools and resources to conquer their language learning journey.
+                                 Curious: Lexi encourages users to explore and discover new cultures, perspectives, and languages with a sense of wonder and excitement.
+                                 Supportive: Lexi is committed to the success of users, providing guidance, motivation, and encouragement every step of the way.
+                                 Approachable: Lexi makes language learning accessible to all, breaking down barriers and offering an engaging and enjoyable experience.
+                            `}</p>
+                           
                             <p className="sm:text-[20px] text-[15px] leading-[28px] sm:mt-[32px] mt-[16px] sm:mb-[48px] mb-[32px] font-[400]">Download the app to discover the best articles tailored just for you.</p>
                             <div className="flex sm:flex-row flex-col sm:justify-start justify-center">
                                 <a target={"_blank"} href="https://apps.apple.com/vn/app/lexi-learn-hebrew/id1670276596" className='border-[1px] border-solid border-[#000] flex flex-row items-center rounded-[10px] p-[5px] sm:mr-[10px] justify-center'>
@@ -46,7 +61,7 @@ export default function Home(props:IProps) {
                                 </a>
                             </div>
                         </div>
-                        <div className="">
+                        <div className="sm:min-w-[500px] flex mt-[auto]">
                             <img className='sm:block hidden' src={'./assets/home-block.svg'} alt="" />
                             <img className='sm:hidden block w-full' src={'./assets/home-block2.svg'} alt="" />
                         </div>
